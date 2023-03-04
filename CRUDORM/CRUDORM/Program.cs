dde7c4cab5943a884c81d04956c27d865dcd7149
TsDafov <dafovtsvetomir@gmail.com>
2023-03-04 20:10:16 +0200

    TheBeginning

---
+++ CRUDORM/CRUDORM/Program.cs
@@ -0,0 +1,17 @@
+﻿using CRUDORM.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display d = new Display();
+        }
+    }
+}

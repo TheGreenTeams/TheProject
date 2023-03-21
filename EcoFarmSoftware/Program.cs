b1943d051422e94872bb3e63f7a5128a6c382836
TsDafov <dafovtsvetomir@gmail.com>
2023-03-21 21:56:42 +0200

    2

--- EcoFarmSoftware/Program.cs
+++ EcoFarmSoftware/Program.cs
@@ -5,14 +5,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheFarm;
+//using System.Windows.Forms;
+
 
 namespace EcoFarmSoftware
 {
     class Program
     {
+        //[STAThread]
         static void Main()
         
         {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
             ConsoleInterface a = new ConsoleInterface();
         }
     }

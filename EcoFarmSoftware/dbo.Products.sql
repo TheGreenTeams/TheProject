9ec81f8d771b216fe9c75ada586765c2e35eabc5
TsDafov <dafovtsvetomir@gmail.com>
2023-03-21 19:06:10 +0200

    1

---
+++ EcoFarmSoftware/dbo.Products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]    INT             IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (MAX)  NULL,
+    [Price] DECIMAL (18, 2) NOT NULL,
+    [Stock] INT             NOT NULL,
+    [Type] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_dbo.Products] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
